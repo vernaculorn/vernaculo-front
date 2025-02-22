d04d55f3bf4cbb66a20e4aca46b0ad01b0232b1e
Carlos <carlosemidiopereira@gmail.com>
2025-02-22 19:17:06 -0300

    Update page.tsx

--- app/acoes/[slug]/page.tsx
+++ app/acoes/[slug]/page.tsx
@@ -20,7 +20,7 @@ export default function Tipografia({ params }: { params: { slug: string } }) {
   return (
     <div className='flex flex-col w-full lg:p-10 md:p-10 pt-0 bg-slate-100 gap-5'>
       <div className='container w-full mx-auto'>
-        <div style={{backgroundImage: `url(${action?.image.file_url})` }} className='bg-no-repeat bg-center bg-cover w-full h-80 flex relative'>
+        <div style={{backgroundImage: `url(${action?.image?.file_url})` }} className='bg-no-repeat bg-center bg-cover w-full h-80 flex relative'>
           <div style={{background: `${action?.primary_color}90`}} className='absolute w-full h-full'></div>
           <div className='flex flex-col w-full h-full absolute z-[1]' style={{color: action?.secondary_color}}>
             <div className='z-10 mt-auto w-full'>
@@ -34,7 +34,7 @@ export default function Tipografia({ params }: { params: { slug: string } }) {
         </div>
         <div className='mx-2 md:mx-0 lg:mx-0'>
           {(action?.files && action?.files.length > 0)
-            ? <Carrossel images={action?.files.map((file, index) => ({src: file.file_url, id: index}))} />
+            ? <Carrossel images={action?.files.map((file, index) => ({src: file?.file_url, id: index}))} />
             : <div className='flex flex-col p-8 border-solid border-4 w-full'>
                 <p>Este projeto não possui imagens</p>
               </div>
@@ -42,7 +42,7 @@ export default function Tipografia({ params }: { params: { slug: string } }) {
         </div>
         <div className='m-5 lg:mx-32 md:mx-10'>
           <div className='flex flex-col p-8 border-solid border-4 w-full' style={{borderColor: action?.primary_color}}>
-            {action && <div style={{ color: '#000000' }} dangerouslySetInnerHTML={{__html: action.content}}></div>}
+            {action && <div style={{ color: '#000000' }} dangerouslySetInnerHTML={{__html: action?.content}}></div>}
           </div>
         </div>
       </div>
